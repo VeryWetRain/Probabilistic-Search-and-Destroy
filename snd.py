9781e4e6eb1c2e745ceacd3d3b56505a60da5c8b
Ryan K <tazze9@gmail.com>
2021-04-07 21:22:46 -0400

    created base file, create environment class

---
+++ snd.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random as rand
+import itertools
+
+class Environment:
+    def __init__(self):
+        # generate 50x50 grid
+        self.map = np.zeros((50,50))
+
+        # each cell randomly generated as flat, hilly, forest, cave with 0.25 chance each
+        # legend for map:
+        # 0: flat
+        # 1: hilly
+        # 2: forest
+        # 3: cave
+        for i in range(50):
+            for j in range(50):
+                prob = rand.uniform(0,1)
+                if prob <= 0.25:
+                    self.map[i][j] = 0
+                elif prob > 0.25 and prob <= 0.5:
+                    self.map[i][j] = 1
+                elif prob > 0.5 and prob <= 0.75:
+                    self.map[i][j] = 2
+                else:
+                    self.map[i][j] = 3
+
+        # randomly choose a cell as the target, doesn't matter where as long as uniformly random
+        x = rand.randint(0, 50)
+        y = rand.randint(0, 50)
+        self.target = (x, y)
+
+class Agent:
+    def __init__(self, environment):
+
+
+
+
+
+
+if __name__ == "__main__":
+    searchanddestroy = Environment()
+    jamesbond = Agent(searchanddestroy)
+    
